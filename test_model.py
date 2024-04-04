3e1844cbe2c2e792fdee9d6811c2b035374e0d32
cfvedova <conorfvedova@gmail.com>
2024-04-04 17:16:30 -0400

    Test best model

--- test_model.py
+++ test_model.py
@@ -539,7 +539,7 @@ def test(model: nn.Module, adata: ad.AnnData) -> float:
 # Test results:
 
 predictions = test(model, adata_test)
-predictions = np.maximum(predictions, 0)
+predictions = np.maximum(predictions, 0)[0]
 results = {}
 print(f"Model Predictions for celltype: {predictions}")
 
