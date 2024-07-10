75ce4f606884c530158ba70a7b8409d65d9c50a7
cfvedova <conorfvedova@gmail.com>
2024-07-10 14:30:34 -0400

    Add debugging print statements

--- scGPT/preprocess.py
+++ scGPT/preprocess.py
@@ -179,9 +179,9 @@ class Preprocessor:
             binned_rows = []
             bin_edges = []
             layer_data = _get_obs_rep(adata, layer=key_to_process)
-            print(layer_data)
+            logger.info(layer_data)
             layer_data = layer_data.A if issparse(layer_data) else layer_data
-            print(layer_data)
+            logger.info(layer_data)
             if layer_data.min() < 0:
                 raise ValueError(
                     f"Assuming non-negative data, but got min value {layer_data.min()}."
@@ -196,9 +196,9 @@ class Preprocessor:
                     binned_rows.append(np.zeros_like(row, dtype=np.int64))
                     bin_edges.append(np.array([0] * n_bins))
                     continue
-                print(row)
+                logger.info(row)
                 non_zero_ids = row.nonzero()
-                print(non_zero_ids)
+                logger.info(non_zero_ids)
                 non_zero_row = row[non_zero_ids]
                 bins = np.quantile(non_zero_row, np.linspace(0, 1, n_bins - 1))
                 # bins = np.sort(np.unique(bins))
@@ -208,7 +208,7 @@ class Preprocessor:
                 assert non_zero_digits.min() >= 1
                 assert non_zero_digits.max() <= n_bins - 1
                 binned_row = np.zeros_like(row, dtype=np.int64)
-                print(non_zero_digits)
+                logger.info(non_zero_digits)
                 binned_row[non_zero_ids] = non_zero_digits
                 binned_rows.append(binned_row)
                 bin_edges.append(np.concatenate([[0], bins]))
