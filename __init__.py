d4a23bf509c71a1d50d5b7bbd2242f18e1161263
cfvedova <conorfvedova@gmail.com>
2024-07-12 12:59:55 -0400

    Fix imports to link to package files directly

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
 import cfGPT.scGPT as scgpt
+import scgpt as scg
+from scgpt.model import TransformerModel, AdversarialDiscriminator
+from scgpt.tokenizer import tokenize_and_pad_batch, random_mask_value
+from scgpt.loss import (
+    masked_mse_loss,
+    masked_relative_error,
+    criterion_neg_log_bernoulli,
+)
+from scgpt.tokenizer.gene_tokenizer import GeneVocab
+from scgpt.preprocess import Preprocessor
+from scgpt import SubsetsBatchSampler
+from scgpt.utils import set_seed, category_str2int, eval_scib_metrics
